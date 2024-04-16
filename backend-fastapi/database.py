a0f8b59501b2bbb6572366f3fdfb46e75c185f91
최성민 <sungmin2517@naver.com>
2024-04-16 12:57:32 +0900

    update

--- backend-fastapi/database.py
+++ backend-fastapi/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # 지금은 sqlite3로 사용
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./capstone.db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 
